c8842a520b5b00c2201742ceeb804f5110e95713
Chris ikongo <chris.ikongo@034gc.onmicrosoft.com>
2020-06-24 15:50:54 -0400

    Azure Blob Storage Unit Test

--- CDNApplication.Test/AzureBlobStorageTests.cs
+++ CDNApplication.Test/AzureBlobStorageTests.cs
@@ -46,48 +46,5 @@ namespace CDNApplication.Test
 
         }
 
-        [Fact]
-        public async void UploadFilesToAzureBlob()
-        {
-
-            var root = Directory.GetParent(Directory.GetCurrentDirectory()).Parent;
-
-            var fileName1 = "random-text.txt";
-
-            var fileName2 = "happy-birthday.png";
-
-            var formFiles = new List<FormFile>();
-
-            string newPath1 = Path.GetFullPath(Path.Combine(root.FullName, @"..\DummyData\", fileName1));
-
-            string newPath2 = Path.GetFullPath(Path.Combine(root.FullName, @"..\DummyData\", fileName2));
-
-            var pathArr = new List<string> { newPath1, newPath2 };
-
-            foreach (var item in pathArr)
-            {
-                Assert.True(File.Exists(item));
-            }
-
-            foreach (var item in pathArr)
-            {
-                using var stream = File.OpenRead(item);
-
-                formFiles.Add(new FormFile(stream, 0, stream.Length, null, Path.GetFileName(stream.Name))
-                {
-                    Headers = new HeaderDictionary(),
-                    ContentType = "application/" + Path.GetExtension(newPath1).Replace(".", "")
-                });
-            }
-
-            var t = new List<IFileListEntry>();
-
-            //var result = await azureBlobService.UploadMultipleFilesAsync(formFiles, "unittests");
-
-
-            //Assert.NotNull(result);
-
-        }
-
     }
 }
