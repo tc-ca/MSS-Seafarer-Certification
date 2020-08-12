904b988a0d470f705e104266a06a917fd64a9599
Alexandre Sanscartier <alex.sanscartier@gmail.com>
2020-08-12 09:50:42 -0400

    Added a dismissable banner to the top of the page

---
+++ CDTS Blazor/Components/Banners/Banner.razor.cs
@@ -0,0 +1,50 @@
+﻿namespace CDNApplication.Components.Banners
+{
+    using System;
+    using Microsoft.AspNetCore.Components;
+    using Microsoft.AspNetCore.Components.Web;
+
+    /// <summary>
+    /// Banner base.
+    /// </summary>
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1649:File name should match first type name", Justification = "In this case we need different class name and file name")]
+    public class BannerBase : ComponentBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BannerBase"/> class.
+        /// </summary>
+        public BannerBase()
+        {
+            this.IsVisible = true;
+        }
+
+        /// <summary>
+        /// Gets or sets the banner Message.
+        /// </summary>
+        [Parameter]
+        public string Message { get; set; }
+
+        /// <summary>
+        /// Gets or sets the phase.
+        /// </summary>
+        [Parameter]
+        public string Phase { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the banner should be visible.
+        /// </summary>
+        [Parameter]
+        public bool IsVisible { get; set; }
+
+        /// <summary>
+        /// Closes the banner.
+        /// </summary>
+        /// <param name="e">The mouse event arguements.</param>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CA1801:Review unused parameters", Justification = "We need this.")]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Globalization", "CA1303:Do not pass literals as localized parameters", Justification = "it's fine here.")]
+        public void CloseBanner(MouseEventArgs e)
+        {
+            this.IsVisible = false;
+        }
+    }
+}
--- CDTS Blazor/Model/SessionStateModel.cs
+++ CDTS Blazor/Model/SessionStateModel.cs
@@ -5,8 +5,14 @@
     /// </summary>
     public class SessionStateModel
     {
+        /// <summary>
+        /// Gets or sets the current language.
+        /// </summary>
         public string CurrentLanguage { get; set; }
 
+        /// <summary>
+        /// Gets or sets the last viewed page.
+        /// </summary>
         public string LastViewedPage { get; set; }
     }
 }
