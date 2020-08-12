b44366046cde6be50513a0d303ce061e653b49c7
Alexandre Sanscartier <alex.sanscartier@gmail.com>
2020-08-12 14:36:11 -0400

    Added css stylings for the banner. Modified the banner to have different types of banners available.

--- CDTS Blazor/Components/Banners/Banner.razor.cs
+++ CDTS Blazor/Components/Banners/Banner.razor.cs
@@ -7,17 +7,8 @@
     /// <summary>
     /// Banner base.
     /// </summary>
-    [System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1649:File name should match first type name", Justification = "In this case we need different class name and file name")]
     public class BannerBase : ComponentBase
     {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="BannerBase"/> class.
-        /// </summary>
-        public BannerBase()
-        {
-            this.IsVisible = true;
-        }
-
         /// <summary>
         /// Gets or sets the banner Message.
         /// </summary>
@@ -31,20 +22,33 @@
         public string Phase { get; set; }
 
         /// <summary>
-        /// Gets or sets a value indicating whether the banner should be visible.
+        /// Gets the classname.
         /// </summary>
-        [Parameter]
-        public bool IsVisible { get; set; }
+        public string ClassName
+        {
+            get
+            {
+                switch (this.State)
+                {
+                    case BannerState.BETA:
+                    case BannerState.ALPHA:
+                        return "prototype";
+                    case BannerState.WARNING:
+                        return "warning";
+                    case BannerState.SUCCESS:
+                        return "success";
+                    case BannerState.ERROR:
+                        return "error";
+                    default:
+                        return string.Empty;
+                }
+            }
+        }
 
         /// <summary>
-        /// Closes the banner.
+        /// Gets or sets the state of the banner.
         /// </summary>
-        /// <param name="e">The mouse event arguements.</param>
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CA1801:Review unused parameters", Justification = "We need this.")]
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Globalization", "CA1303:Do not pass literals as localized parameters", Justification = "it's fine here.")]
-        public void CloseBanner(MouseEventArgs e)
-        {
-            this.IsVisible = false;
-        }
+        [Parameter]
+        public BannerState State { get; set; }
     }
 }
