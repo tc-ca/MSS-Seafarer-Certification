da126b3fc2ea6fa2e0f9938c8337a2c0bee8266d
Alexandre Sanscartier <alex.sanscartier@gmail.com>
2020-09-18 15:13:32 -0400

    work for the PBI AB#44949. Put the stepper into its own component. Created translations for the stepper

---
+++ CDTS Blazor/TCComponents/Stepper/BaseStepper.cs
@@ -0,0 +1,35 @@
+﻿namespace CDNApplication.TCComponents.Stepper
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class BaseStepper : IStepper
+    {
+        private List<IStep> steps;
+
+        public int ActiveIndex { get; set; }
+
+        public int AmountOfSteps
+        {
+            get
+            {
+                return this.steps.Count;
+            }
+        }
+
+        public BaseStepper()
+        {
+            this.steps = new List<IStep>();
+        }
+
+        public IStep GetStepAtIndex(int index)
+        {
+            return this.steps[index];
+        }
+
+        public void AddStep(IStep step)
+        {
+            this.steps.Add(step);
+        }
+    }
+}
