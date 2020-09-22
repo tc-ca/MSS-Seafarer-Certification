dc3864f3346fe48b41430b667aef73761add05ba
Alexandre Sanscartier <alex.sanscartier@gmail.com>
2020-09-22 13:02:24 -0400

    Added a service to create the stepper for the upload documents. Fixed merge conflicts.

--- CDTS Blazor/TCComponents/Stepper/BaseStepper.cs
+++ CDTS Blazor/TCComponents/Stepper/BaseStepper.cs
@@ -22,6 +22,40 @@
             this.steps = new List<IStep>();
         }
 
+        public void ActivateStepAtIndex(int index)
+        {
+            var currentStep = this.GetStepAtIndex(index);
+            for (int i = 0; i < index; i++)
+            {
+                var previousStep = this.GetStepAtIndex(i);
+                this.setStepAsComplete(previousStep);
+            }
+            for (int i = index; i < this.AmountOfSteps - 1; i++)
+            {
+                var nextStep = this.GetStepAtIndex(i);
+                this.setStepAsInactive(nextStep);
+            }
+            this.setStepAsActive(currentStep);
+        }
+
+        private void setStepAsComplete(IStep step)
+        {
+            step.IsActive = false;
+            step.IsComplete = true;
+        }
+
+        private void setStepAsInactive(IStep step)
+        {
+            step.IsActive = false;
+            step.IsComplete = false;
+        }
+
+        private void setStepAsActive(IStep step)
+        {
+            step.IsActive = true;
+            step.IsComplete = false;
+        }
+
         public IStep GetStepAtIndex(int index)
         {
             return this.steps[index];
