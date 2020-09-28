3d7c4a71738c8ee0b09dc6490a35e7ff550eda22
TCPrimedPaul <paul.delannoy@tc.gc.ca>
2020-09-28 13:56:23 -0400

    UseHttpsRedirection and set the https_port in appsettings.json.

--- CDTS Blazor/Startup.cs
+++ CDTS Blazor/Startup.cs
@@ -126,8 +126,8 @@ namespace CDNApplication
                 app.UseExceptionHandler("/Error");
 
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                //app.UseHsts();
-                //app.UseHttpsRedirection();
+                app.UseHsts();
+                app.UseHttpsRedirection();
             }
 
 
