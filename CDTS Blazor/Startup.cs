50ec42aae005b234c10b81eab83ce6550f462204
TCPrimedPaul <paul.delannoy@tc.gc.ca>
2020-09-28 14:49:57 -0400

    Add app.UseFowardedHeaders().

--- CDTS Blazor/Startup.cs
+++ CDTS Blazor/Startup.cs
@@ -97,11 +97,11 @@ namespace CDNApplication
             services
                 .ConfigureGoCTemplateRequestLocalization(); // if GoC.WebTemplate-Components.Core (in NuGet) >= v2.1.1
 
-            services.AddHttpsRedirection(options =>
-            {
-                //options.RedirectStatusCode = StatusCodes.Status308PermanentRedirect;
-                options.HttpsPort = 443;
-            });
+            //services.AddHttpsRedirection(options =>
+            //{
+            //    options.RedirectStatusCode = StatusCodes.Status308PermanentRedirect;
+            //    options.HttpsPort = 443;
+            //});
         }
 
         /// <summary>
@@ -127,6 +127,7 @@ namespace CDNApplication
 
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
+                app.UseForwardedHeaders();
                 app.UseHttpsRedirection();
             }
 
