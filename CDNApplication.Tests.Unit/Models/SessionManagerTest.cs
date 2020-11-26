1b38092dde3b618c0ace5e0f9a9c75dc9199342b
AlexandreSanscartier <alexandre.sanscartier@codefor.ca>
2020-11-26 16:35:32 -0500

    [feature/cto-unit-tests] Added a test class for the SessionManager.

---
+++ CDNApplication.Tests.Unit/Models/SessionManagerTest.cs
@@ -0,0 +1,71 @@
+﻿namespace CDNApplication.Tests.Unit.EmailTemplate
+{
+    using CDNApplication.Models;
+    using Xunit;
+
+    public class SessionManagerTest
+    {
+        [Fact]
+        public void SessionManager_CorrectlySetsSessionStateModel()
+        {
+            // Arrange
+            var sessionStateModel = new SessionStateModel()
+            {
+                CurrentLanguage = "en",
+                LastViewedPage = string.Empty,
+            };
+            var sessionManager = new SessionManager(sessionStateModel);
+            var expectedLanguage = "fr";
+            var expectedLastViewedPage = "index";
+
+            // Act
+            sessionManager.UpdateSessionState(expectedLanguage, expectedLastViewedPage);
+
+            // Assert
+            Assert.Equal(expectedLanguage, sessionStateModel.CurrentLanguage);
+            Assert.Equal(expectedLastViewedPage, sessionStateModel.LastViewedPage);
+        }
+
+        [Fact]
+        public void SessionManager_CorrectlySetsSessionStateModel_WhenOnlyLastViewedPage()
+        {
+            // Arrange
+            var expectedLanguage = "en";
+            var sessionStateModel = new SessionStateModel()
+            {
+                CurrentLanguage = expectedLanguage,
+                LastViewedPage = string.Empty,
+            };
+            var sessionManager = new SessionManager(sessionStateModel);
+            var expectedLastViewedPage = "index";
+
+            // Act
+            sessionManager.UpdateSessionState(string.Empty, expectedLastViewedPage);
+
+            // Assert
+            Assert.Equal(expectedLanguage, sessionStateModel.CurrentLanguage);
+            Assert.Equal(expectedLastViewedPage, sessionStateModel.LastViewedPage);
+        }
+
+        [Fact]
+        public void SessionManager_CorrectlySetsSessionStateModel_WhenOnlyLanguage()
+        {
+            // Arrange
+            var expectedLastViewedPage = "index";
+            var sessionStateModel = new SessionStateModel()
+            {
+                CurrentLanguage = string.Empty,
+                LastViewedPage = expectedLastViewedPage,
+            };
+            var expectedLanguage = "en";
+            var sessionManager = new SessionManager(sessionStateModel);
+
+            // Act
+            sessionManager.UpdateSessionState(expectedLanguage, string.Empty);
+
+            // Assert
+            Assert.Equal(expectedLanguage, sessionStateModel.CurrentLanguage);
+            Assert.Equal(expectedLastViewedPage, sessionStateModel.LastViewedPage);
+        }
+    }
+}
