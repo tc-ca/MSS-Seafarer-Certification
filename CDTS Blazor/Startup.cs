5b4caa77498499375a7f3a4a3161e347998b067b
Chris ikongo <chris.ikongo@034gc.onmicrosoft.com>
2020-12-02 15:56:25 -0500

    [AB#49296-Rework-add-document-section] Added UploadFile validation but still needs to be worked on

--- CDTS Blazor/Startup.cs
+++ CDTS Blazor/Startup.cs
@@ -114,6 +114,8 @@ namespace CDNApplication
 
             services.AddTransient<LayoutViewModel>();
             services.AddTransient<IValidator<UploadDocumentPageModel>, UploadDocumentValidator>();
+            services.AddTransient<IValidator<UploadedFile>, UploadedFileValidator>();
+
             services.AddTransient<IMtoaServices, MtoaServices>();
             services.AddLocalization(options => options.ResourcesPath = "Resources");
             services.Configure<RequestLocalizationOptions>(options =>
