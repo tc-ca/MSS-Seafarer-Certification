9d7c7a6b118d2f710ff32b02a5b45b85322f6be0
AlexandreSanscartier <alexandre.sanscartier@codefor.ca>
2020-12-29 17:04:55 -0500

    [feature/cto-unit-tests] [feature/cto-unit-tests] Create tests for the SessionHelper class.

---
+++ CDNApplication.Tests.Unit/Utilities/ServiceLocatorTests.cs
@@ -0,0 +1,48 @@
+﻿namespace CDNApplication.Tests.Unit.Utilities
+{
+    using CDNApplication.Utilities;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.Logging;
+    using Moq;
+    using System;
+    using Xunit;
+
+    public class ServiceLocatorTests
+    {
+        public ServiceLocatorTests()
+        {
+
+        }
+
+        [Fact]
+        public void GetServiceUri_ReturnsServiceCorrectly()
+        {
+            // Arrange
+            var configMock = new Mock<IConfiguration>();
+            configMock.Setup(config => config.GetSection("ServiceLocatorEndpoints")["Mtoa"]).Returns("https://localhost/mtoaapi");
+            var loggerMock = new Mock<ILogger<ServiceLocator>>();
+            var expectedResult = new Uri("https://localhost/mtoaapi");
+
+            var serviceLocator = new ServiceLocator(configMock.Object, loggerMock.Object);
+
+            // Act
+            var serviceUri = serviceLocator.GetServiceUri(ServiceLocatorDomain.Mtoa);
+
+            // Assert
+            Assert.Equal(expectedResult, serviceUri);
+        }
+
+        [Fact]
+        public void GetServiceUri_WhenKeyDoesNotExist_ThrowsNullReferenceException()
+        {
+            // Arrange
+            var configMock = new Mock<IConfiguration>();
+            var loggerMock = new Mock<ILogger<ServiceLocator>>();
+
+            var serviceLocator = new ServiceLocator(configMock.Object, loggerMock.Object);
+
+            // Assert
+            Assert.Throws<NullReferenceException>(() => serviceLocator.GetServiceUri(ServiceLocatorDomain.Mtoa));
+        }
+    }
+}
