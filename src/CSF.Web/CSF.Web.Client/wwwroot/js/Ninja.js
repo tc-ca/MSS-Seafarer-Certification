18eb633da90a79ebba7311349abd397d4a5f8bfa
mansuer.aizimu <mansuer.aizimu@tc.gc.ca>
2021-01-04 08:52:04 -0500

    AB#54500- Added Google reCaptcha element

--- src/CSF.Web/CSF.Web.Client/wwwroot/js/Ninja.js
+++ src/CSF.Web/CSF.Web.Client/wwwroot/js/Ninja.js
@@ -3,6 +3,39 @@
     document.getElementById("Document").click();
 }
 
+var onloadCallback = function ()
+{
+    var recaptcha_tag = document.getElementById("reCapture_element");
+    // if reCapture_element is not rendered yet, we will render this element.
+    if (recaptcha_tag.hasChildNodes.length == 0)
+    {
+        grecaptcha.render('reCapture_element', {
+            'sitekey': '6LdLxhoaAAAAABr4xFKLYiojPjlt9LiXI3iDTXfk',
+            'callback': 'onRecaptchaSubmit',
+            'theme': 'light'
+        }
+        );
+    }
+};
+
+function onRecaptchaSubmit()
+{
+    return true;
+}
+
+function isRecaptchaChecked()
+{
+    var isChecked = null;
+
+    if (grecaptcha && grecaptcha.getResponse().length > 0)
+    {
+        //alert('Well, recaptcha is checked !');
+        isChecked = "YES";
+    }
+
+    return isChecked;
+}
+
 window.initPopover = () => {
     $('[data-toggle="popover"]').popover({
         html:true,
