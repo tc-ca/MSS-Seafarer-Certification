ee05032db8eb076453e977a16ea39f723768a54d
Chris ikongo <chris.ikongo@034gc.onmicrosoft.com>
2021-01-20 14:51:14 -0500

    [AB#56267-Build-a-List-of-Values-service] AB#57001 & AB#56999 Just checking in changes

---
+++ CSF.MPDIS.API.Unit/Controllers/CertificateControllerTest.cs
@@ -0,0 +1,37 @@
+﻿using CSF.MPDIS.API.Controllers;
+using CSF.MPDIS.API.Data.Entity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text.Json.Serialization;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http.Results;
+using Xunit;
+
+namespace CSF.Web.Client.Tests.Unit.Controllers
+{
+    public class CertificateControllerTest
+    {
+
+        [Fact]
+        public void RestClient_GetCertificates_Succeeds()
+        {
+            
+            var cr = new CertificateController();
+
+            var res = cr.GetCertificates();
+
+            var t = res as OkObjectResult;
+
+            Assert.NotNull(t.Value);
+
+        }
+
+
+    }
+}
---
+++ src/CSF.MPDIS/CSF.MPDIS.API/Controllers/CertificateController.cs
@@ -0,0 +1,24 @@
+﻿using CSF.MPDIS.API.Data.Entity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSF.MPDIS.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CertificateController : ControllerBase
+    {
+
+        [HttpGet]
+        [Route("")]
+        public IActionResult GetCertificates()
+        {
+
+            return Ok(CertificateType.GetAllCertificateTypes());
+        }
+
+    }
+}
