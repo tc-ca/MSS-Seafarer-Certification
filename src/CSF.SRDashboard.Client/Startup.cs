bf7a4c65c2560a0463c210d82ed137026ce29dbe
AlexandreSanscartier <alexandre.sanscartier@codefor.ca>
2021-02-04 12:19:26 -0500

    [AB#34108-Add-application-insights] AB#65432 Added Application insights to the dashboard project.

--- src/CSF.SRDashboard.Client/Startup.cs
+++ src/CSF.SRDashboard.Client/Startup.cs
@@ -27,6 +27,7 @@ namespace CSF.SRDashboard.Client
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
+            services.AddApplicationInsightsTelemetry(Configuration["APPINSIGHTS_CONNECTIONSTRING"]);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
