b9da1fad8c6c1d25d8cd50c467889d00ef1698fb
AlexandreSanscartier <alexandre.sanscartier@codefor.ca>
2021-02-17 14:56:27 -0500

    [AB#67483-Create-request-details-screen] AB#67691 AB#67695 Updated styling to reflect what is in figma. Added default content to the Summary tab to show where elements would go. Built the overall layout and modified the tab control to allow adding the status badge at the end.

---
+++ src/CSF.SRDashboard.Client/Pages/ServiceDetails.razor.cs
@@ -0,0 +1,13 @@
+﻿namespace CSF.SRDashboard.Client.Pages
+{
+    using Microsoft.AspNetCore.Components;
+
+    public partial class ServiceDetails
+    {
+        /// <summary>
+        /// Gets or sets the service request identification number.
+        /// </summary>
+        [Parameter]
+        public int ServiceRequestId { get; set; }
+    }
+}
