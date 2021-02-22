0641bb80e5a5b7863ef583f9da997b3d08610322
Chris ikongo <chris.ikongo@034gc.onmicrosoft.com>
2021-02-22 15:02:57 -0500

    [AB#67485-Create-request-details-screen-component-request-details] AB#67704 - Integrate / Implement component & Created card component

---
+++ src/CSF.Components/Layouts/Card.razor.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSF.Components.Layouts
+{
+    public partial class Card
+    {
+
+        /// <summary>
+        /// Gets or sets the Header render fragment of the Card.
+        /// </summary>
+        [Parameter]
+        public RenderFragment Header { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Body render fragment of the Card.
+        /// </summary>
+        [Parameter]
+        public RenderFragment Body { get; set; }
+
+        [Parameter]
+        public string HeaderClasses { get; set; }
+
+        [Parameter]
+        public string HeaderStyle { get; set; }
+
+        [Parameter]
+        public string BodyClasses { get; set; }
+
+        [Parameter]
+        public string BodyStyle { get; set; }
+    }
+}
