15b3cfb404d79778f95a013c015a629994173807
Alexandre Sanscartier <alex.sanscartier@gmail.com>
2021-02-24 17:11:51 -0500

    Ab#67487 create request details screen component attachments (#136)
    * [AB#67485-Create-request-details-screen-component-request-details] AB#67704 - Integrate / Implement component & Created card component
    
    * [AB#67487-Create-request-details-screen-component-attachments] AB#67713 - Get data from service request
    
    * [AB#67487-Create-request-details-screen-component-attachments] AB#67693 - AB#67697 Implemented the screen and files
    
    * [AB#67487-Create-request-details-screen-component-attachments] AB#67697 Fixed styling issues. Added ability to download file.
    
    * [AB#67487-Create-request-details-screen-component-attachments] [AB#67487-Create-request-details-screen-component-attachments] Fixed merge errors.
    
    * [AB#67487-Create-request-details-screen-component-attachments] Removed the FileList class as it was not being used.
    
    Co-authored-by: Chris ikongo <chris.ikongo@034gc.onmicrosoft.com>

--- src/CSF.SRDashboard.Client/Components/Attachments.razor.cs
+++ src/CSF.SRDashboard.Client/Components/Attachments.razor.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using CSF.Web.Client.Models.PageModels;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Hosting;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +11,35 @@ namespace CSF.SRDashboard.Client.Components
 {
     public partial class Attachments
     {
+        [Inject]
+        private IWebHostEnvironment hostingEnv { get; set; }
+
+        public List<UploadedFile> UploadedFiles { get; set; }
+
+        protected override void OnAfterRender(bool firstRender)
+        {
+            if (firstRender)
+            {
+                var h = hostingEnv.WebRootPath;
+
+                string FilePath = Path.Combine(hostingEnv.WebRootPath, "temp");
+
+                string[] filePaths = Directory.GetFiles(FilePath);
+
+                UploadedFiles = new List<UploadedFile>();
+
+                foreach (string filePath in filePaths)
+                {
+                    UploadedFiles.Add(new UploadedFile
+                    {
+                        FilePath = filePath,
+                        FileName = Path.GetFileName(filePath)
+                    });
+                }
+            }
+            StateHasChanged();
+            base.OnAfterRender(firstRender);
+        }
+
     }
 }
