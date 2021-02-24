2ffaeda48f446a7c8707f22a5409caa34d845913
Chris Ikongo <64083746+tc-chris@users.noreply.github.com>
2021-02-24 13:33:58 -0500

    Ab#67485 create request details screen component request details (#135)
    * [AB#67485-Create-request-details-screen-component-request-details] AB#67704 - Integrate / Implement component & Created card component
    
    * [AB#67487-Create-request-details-screen-component-attachments] AB#67713 - Get data from service request
    
    Co-authored-by: Alexandre Sanscartier <alex.sanscartier@gmail.com>

---
+++ src/CSF.SRDashboard.Client/Components/RequestDetail.razor.cs
@@ -0,0 +1,17 @@
+﻿using CSF.SRDashboard.Client.Models;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSF.SRDashboard.Client.Components
+{
+    public partial class RequestDetail
+    {
+
+        [Parameter]
+        public RequestDetailComponentModel Model { get; set; }
+
+    }
+}
--- src/CSF.SRDashboard.Client/Pages/ServiceDetails.razor.cs
+++ src/CSF.SRDashboard.Client/Pages/ServiceDetails.razor.cs
@@ -1,13 +1,37 @@
 ﻿namespace CSF.SRDashboard.Client.Pages
 {
+    using CSF.SRDashboard.Client.Models;
+    using CSF.SRDashboard.Client.Services;
     using Microsoft.AspNetCore.Components;
 
     public partial class ServiceDetails
     {
+        [Inject]
+        public IMtoaArtifactService MtoaArtifactService { get; set; }
+
+        public RequestDetailComponentModel RequestDetailComponentModel { get; set; }
         /// <summary>
         /// Gets or sets the service request identification number.
         /// </summary>
         [Parameter]
         public int ServiceRequestId { get; set; }
+
+        protected override void OnAfterRender(bool firstRender)
+        {
+            if (firstRender)
+            {
+                var model = this.MtoaArtifactService.GetArtifactByServiceRequestId(this.ServiceRequestId);
+
+                this.RequestDetailComponentModel = new RequestDetailComponentModel
+                {
+                    CertificateType = model.CertificateType,
+                    RequestType = "Marine medical certificate Dummy",
+                    TriageType = "Fast Track Dummy"
+                };
+
+                InvokeAsync(StateHasChanged);
+            }
+            base.OnAfterRender(firstRender);
+        }
     }
 }
