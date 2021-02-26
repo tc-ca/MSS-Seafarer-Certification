aefa33de5d19c56eb0a48846ff85870c7b23d6cd
Chris ikongo <chris.ikongo@034gc.onmicrosoft.com>
2021-02-26 12:15:44 -0500

    [AB#67489-Create-request-details-screen-component-mme-details] AB#69300 - Implement component

---
+++ src/CSF.SRDashboard.Client/Components/MMEDetails.razor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSF.SRDashboard.Client.Components
+{
+    public partial class MMEDetails
+    {
+
+        protected override void OnAfterRender(bool firstRender)
+        {
+
+        }
+
+    }
+}
