ab44419b877acb6005406264a97400432f622773
TC-Cory <cory.chenier@tc.gc.ca>
2021-04-19 11:07:10 -0400

    [AB#81435-Find-Seafarer] Update to find seafarer
    Implemented separate results page

--- src/CSF.SRDashboard.Client/Pages/FindSeafarer.razor.cs
+++ src/CSF.SRDashboard.Client/Pages/FindSeafarer.razor.cs
@@ -1,4 +1,5 @@
-﻿using DSD.MSS.Blazor.Components.Table;
+﻿using CSF.SRDashboard.Client.Services;
+using DSD.MSS.Blazor.Components.Table;
 using DSD.MSS.Blazor.Components.Table.Models;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Forms;
@@ -19,22 +20,18 @@ namespace CSF.SRDashboard.Client.Pages
 
         [Inject]
         public IMpdisService MpdisService { get; set; }
-        protected Table<ApplicantSearchResultItem> TableRef { get; set; }
-        protected List<ApplicantSearchResultItem> tableData = new List<ApplicantSearchResultItem>();
+        [Inject]
+        public IResultsService resultService { get; set; }
         public ApplicantSearchResult searchResult { get; set; }
         public string json { get; set; }
-        public bool ShowFilterHeader { get; set; } = true;
-        private const string tableSettingkey = "TableSettings";
-        protected TableSettings<ApplicantSearchResultItem> tableSettings { get; set; }
+        [Inject]
+        NavigationManager navigationManager { get; set; }
         public ApplicantSearchCriteria searchCriteria = new ApplicantSearchCriteria();
-        public bool showTable { get; set; } = true;
-        public bool showForm { get; set; } = false;
-        private IMemoryCache memoryCache;
-
+  
         protected override void OnInitialized()
         {
             base.OnInitialized();
-           
+
             editContext = new EditContext(searchCriteria);
             var isValid = editContext.Validate();
 
@@ -43,49 +40,11 @@ namespace CSF.SRDashboard.Client.Pages
 
         public void Search()
         {
-
-            showTable = false;
-            showForm = true;
-            var result =  this.MpdisService.Search(searchCriteria);
-            
-            tableData = (List<ApplicantSearchResultItem>)LoadData(result);
-            //TableRef.Update();
-
+            var result = this.MpdisService.Search(searchCriteria);
+            resultService.LoadData(result);
+            navigationManager.NavigateTo("/SearchResults");
             json = JsonConvert.SerializeObject(result.Items, Formatting.Indented);
 
-        }
-       private IEnumerable<ApplicantSearchResultItem> LoadData(ApplicantSearchResult result) 
-        {
-        List<ApplicantSearchResultItem> list = result.Items;
-
-            return list;
-        }
-        
-        
-        protected void OnAfterTableDataLoaded()
-        {
-            if (tableSettings != null)
-            {
-                TableRef.ResetTableSettings(tableSettings);
-                tableSettings = null;
-            }
-        }
-        public void OnFilterChanged(TableSettings<ApplicantSearchResultItem> settings)
-        {
-
-            var cacheOptions = new MemoryCacheEntryOptions()
-            {
-                AbsoluteExpiration = DateTime.Now.AddHours(2)
-            };
-
-            if (TableRef != null)
-            {
-                memoryCache.Set("TableSettings", settings);
-            }
-        }
-        protected void HandleHeaderFilterChanged()
-        {
-            StateHasChanged();
         }
 
     }
---
+++ src/CSF.SRDashboard.Client/Pages/SearchResults.razor.cs
@@ -0,0 +1,59 @@
+﻿using CSF.SRDashboard.Client.Services;
+using DSD.MSS.Blazor.Components.Table;
+using DSD.MSS.Blazor.Components.Table.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Caching.Memory;
+using MPDIS.API.Wrapper.Services.MPDIS.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSF.SRDashboard.Client.Pages
+{
+   public partial class SearchResults
+    {
+        [Inject]
+        public IResultsService resultService { get; set; }
+        protected TableSettings<ApplicantSearchResultItem> tableSettings { get; set; }
+        public bool ShowFilterHeader { get; set; } = true;
+        private const string tableSettingkey = "TableSettings";
+        protected Table<ApplicantSearchResultItem> TableRef { get; set; }
+        protected List<ApplicantSearchResultItem> tableData = new List<ApplicantSearchResultItem>();
+        private IMemoryCache memoryCache;
+       
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            tableData = resultService.list;
+
+        }
+        protected void OnAfterTableDataLoaded()
+        {
+            if (tableSettings != null)
+            {
+                TableRef.ResetTableSettings(tableSettings);
+                tableSettings = null;
+            }
+        }
+        public void OnFilterChanged(TableSettings<ApplicantSearchResultItem> settings)
+        {
+
+            var cacheOptions = new MemoryCacheEntryOptions()
+            {
+                AbsoluteExpiration = DateTime.Now.AddHours(2)
+            };
+
+            if (TableRef != null)
+            {
+                memoryCache.Set("TableSettings", settings);
+            }
+        }
+        protected void HandleHeaderFilterChanged()
+        {
+            StateHasChanged();
+        }
+
+    }
+}
+
