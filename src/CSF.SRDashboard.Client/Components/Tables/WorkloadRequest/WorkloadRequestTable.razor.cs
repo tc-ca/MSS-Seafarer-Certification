d0f5d2760822de74f565596005b219aab1008381
AlexandreSanscartier <alexandre.sanscartier@codefor.ca>
2021-04-22 11:41:19 -0400

    [AB#82348-View-Seafarer-Requests] Created the component for the WorkloadRequestTable. Created extensions to help working with Enums and Strings. Adsded bootstrap javascript to the project.

---
+++ src/CSF.SRDashboard.Client/Components/Tables/WorkloadRequest/WorkloadRequestTable.razor.cs
@@ -0,0 +1,53 @@
+﻿namespace CSF.SRDashboard.Client.Components.Tables.WorkloadRequest
+{
+    using System;
+    using System.Collections.Generic;
+    using Microsoft.AspNetCore.Components;
+    using CSF.Common.Library.Extensions.String;
+    using CSF.SRDashboard.Client.Services.WorkloadRequest.Enums;
+    using CSF.SRDashboard.Client.Components.Tables.WorkloadRequest.Entities;
+
+    public partial class WorkloadRequestTable
+    {
+        [Parameter]
+        public List<WorkloadRequestTableItem> TableData { get; set; } = new List<WorkloadRequestTableItem>();
+
+        protected override void OnInitialized()
+        {
+            this.TableData.Add(new WorkloadRequestTableItem()
+            {
+                RequestId = "123456D",
+                Certificate = "Marine medical certificate - 2 years",
+                RequestType = "Renewal",
+                RequestDate = DateTime.Now,
+                Status = "In Progress".ToEnum<RequestStatus>(RequestStatus.UNKNOWN),
+                Priority = "Request more info",
+                Language = "E"
+            });
+            this.TableData.Add(new WorkloadRequestTableItem()
+            {
+                RequestId = "456326H",
+                Certificate = "Marine medical certificate - 2 years",
+                RequestType = "Renewal",
+                RequestDate = DateTime.Now,
+                Status = "Pending".ToEnum<RequestStatus>(RequestStatus.UNKNOWN),
+                Priority = "Missing info",
+                Language = "E"
+            });
+            this.TableData.Add(new WorkloadRequestTableItem()
+            {
+                RequestId = "8721455C",
+                Certificate = "Marine medical certificate - 2 years",
+                RequestType = "New",
+                RequestDate = DateTime.Now,
+                Status = "Complete".ToEnum<RequestStatus>(RequestStatus.UNKNOWN),
+                Priority = "Received",
+                Language = "E"
+            });
+        }
+
+        protected void OnAfterTableDataLoaded()
+        {
+        }
+    }
+}
