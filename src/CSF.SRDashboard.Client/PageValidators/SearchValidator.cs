aedff7801fc4f8e6557f941c687c4d186ce73b0c
TC-Cory <cory.chenier@tc.gc.ca>
2021-04-22 11:10:35 -0400

    [AB#81438-View-Seafarer-Info] Adding validator

---
+++ src/CSF.SRDashboard.Client/PageValidators/SearchValidator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentValidation;
+using MPDIS.API.Wrapper.Services.MPDIS.Entities;
+
+namespace CSF.SRDashboard.Client.PageValidators
+{
+    public class SearchValidator : AbstractValidator<ApplicantSearchCriteria>
+    {
+        public SearchValidator()
+        {
+
+            this.RuleFor(x => x).Cascade(CascadeMode.Stop).NotEmpty();
+           
+
+        }
+    }
+}
