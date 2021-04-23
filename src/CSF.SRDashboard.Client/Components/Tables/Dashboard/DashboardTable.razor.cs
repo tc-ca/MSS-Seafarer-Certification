1f499562a7d56b079b4aaa9ae9d8afc4fa85db88
AlexandreSanscartier <alexandre.sanscartier@codefor.ca>
2021-04-23 10:36:13 -0400

    [AB#81438-View-Seafarer-Info] Created componenty for the DashboardTable.

---
+++ src/CSF.SRDashboard.Client/Components/Tables/Dashboard/DashboardTable.razor.cs
@@ -0,0 +1,35 @@
+﻿namespace CSF.SRDashboard.Client.Components.Tables.Dashboard
+{
+    using System;
+    using System.Collections.Generic;
+    using CSF.SRDashboard.Client.Models;
+    using Microsoft.AspNetCore.Components;
+
+    public partial class DashboardTable
+    {
+        [Parameter]
+        public List<Document> TableData { get; set; } = new List<Document>();
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            TableData.Add(new Document
+            {
+                FileName = "mmeReport.pdf",
+                Language = "English",
+                RequestID = "123456",
+                Type = "MME Report",
+                DateUploaded = new DateTime(2020, 2, 10)
+            });
+
+            TableData.Add(new Document
+            {
+                FileName = "EyeExam.pdf",
+                Language = "French",
+                RequestID = "654321",
+                Type = "Test results",
+                DateUploaded = new DateTime(2019, 12, 14)
+            });
+        }
+    }
+}
--- src/CSF.SRDashboard.Client/Pages/SeafarerProfile.razor.cs
+++ src/CSF.SRDashboard.Client/Pages/SeafarerProfile.razor.cs
@@ -2,16 +2,9 @@
 using MPDIS.API.Wrapper.Services.MPDIS.Entities;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Components.Forms;
 using MPDIS.API.Wrapper.Services.MPDIS;
-using Microsoft.JSInterop;
-using Microsoft.AspNetCore.Components.Routing;
 using CSF.SRDashboard.Client.Models;
 using DSD.MSS.Blazor.Components.Table;
-using DSD.MSS.Blazor.Components.Table.Models;
-using Microsoft.Extensions.Caching.Memory;
 
 namespace CSF.SRDashboard.Client.Pages
 {
@@ -19,67 +12,19 @@ namespace CSF.SRDashboard.Client.Pages
     {
         [Parameter]
         public string Cdn { get; set; }
+
         [Inject]
         public IMpdisService MpdisService { get; set; }
         public ApplicantInformation applicant = new ApplicantInformation();
         public DateTime DOB;
-        protected TableSettings<Document> tableSettings { get; set; }
-        public bool ShowFilterHeader { get; set; } = true;
         protected Table<Document> TableRef { get; set; }
         protected List<Document> TableData = new List<Document>();
-        private readonly IMemoryCache memoryCache;
         public string fullDob { get; set; }
-        [Inject] 
-        NavigationManager navigationManager { get; set; }
         public string currentRelativePath;
-      
 
-        protected override void OnInitialized()
+        protected void OnInitialized()
         {
-            base.OnInitialized();
             this.LoadData();
-            TableData.Add(new Document
-            {
-                FileName = "mmeReport.pdf", 
-                Language = "English", 
-                RequestID = "123456",
-                Type = "MME Report",
-                DateUploaded =  new DateTime(2020,2,10)
-            });
-
-            TableData.Add(new Document
-            {
-                FileName = "EyeExam.pdf",
-                Language = "French",
-                RequestID = "654321",
-                Type = "Test results",
-                DateUploaded = new DateTime(2019, 12, 14)
-            });
-        }
-
-        protected void OnAfterTableDataLoaded()
-        {
-            if (tableSettings != null)
-            {
-                TableRef.ResetTableSettings(tableSettings);
-                tableSettings = null;
-            }
-        }
-        public void OnFilterChanged(TableSettings<Document> settings)
-        {
-            var cacheOptions = new MemoryCacheEntryOptions()
-            {
-                AbsoluteExpiration = DateTime.Now.AddHours(2)
-            };
-
-            if (TableRef != null)
-            {
-                memoryCache.Set("TableSettings", settings);
-            }
-        }
-        protected void HandleHeaderFilterChanged()
-        {
-            StateHasChanged();
         }
 
         private void LoadData()
