c5366641fc1cf6017e0c251b5d706d6a27a0e039
AlexandreSanscartier <alexandre.sanscartier@codefor.ca>
2021-04-27 12:19:21 -0400

    [AB#85233-Search-spinner] Created the spinner.

--- src/CSF.SRDashboard.Client/Pages/FindSeafarer.razor.cs
+++ src/CSF.SRDashboard.Client/Pages/FindSeafarer.razor.cs
@@ -1,20 +1,11 @@
-﻿using CSF.SRDashboard.Client.Services;
-using CSF.SRDashboard.Client.Utilities;
-using DSD.MSS.Blazor.Components.Table;
-using DSD.MSS.Blazor.Components.Table.Models;
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Forms;
-using Microsoft.Extensions.Caching.Memory;
-using MPDIS.API.Wrapper.Services.MPDIS;
-using MPDIS.API.Wrapper.Services.MPDIS.Entities;
-using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace CSF.SRDashboard.Client.Pages
+﻿namespace CSF.SRDashboard.Client.Pages
 {
+    using CSF.SRDashboard.Client.Utilities;
+    using Microsoft.AspNetCore.Components;
+    using Microsoft.AspNetCore.Components.Forms;
+    using MPDIS.API.Wrapper.Services.MPDIS;
+    using MPDIS.API.Wrapper.Services.MPDIS.Entities;
+
     public partial class FindSeafarer
     {
         protected EditContext EditContext;
@@ -36,7 +27,6 @@ namespace CSF.SRDashboard.Client.Pages
 
         public bool error { get; set; } = true;
 
-
         protected override void OnInitialized()
         {
             base.OnInitialized();
@@ -50,18 +40,15 @@ namespace CSF.SRDashboard.Client.Pages
             EditContext = new EditContext(SearchCriteria);
         }
 
-      /// <summary>
-      /// Runs a search after the criteria is met
-      /// </summary>
+          /// <summary>
+          /// Runs a search after the criteria is met
+          /// </summary>
         public void Search()
         {
                 buttonDisabled = "disabled";
-                
-                State.SearchCriteria = SearchCriteria;
-
-                State.ApplicantSearchResult = MpdisService.Search(SearchCriteria);
-
-                NavigationManager.NavigateTo("/SearchResults");
+                //State.SearchCriteria = SearchCriteria;
+                //State.ApplicantSearchResult = MpdisService.Search(SearchCriteria);
+                //NavigationManager.NavigateTo("/SearchResults");
         }     
     }
 }
