33cbcd74e3652fbb4e1280eb31bf8d9eeae65b05
Chris ikongo <chris.ikongo@034gc.onmicrosoft.com>
2021-05-13 09:01:24 -0400

    [AB#86950-Create-New-Request] Created the page for create new request

---
+++ src/CSF.SRDashboard.Client/Pages/CreateNewRequest.razor.cs
@@ -0,0 +1,30 @@
+﻿using CSF.SRDashboard.Client.DTO;
+using CSF.SRDashboard.Client.Services;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSF.SRDashboard.Client.Pages
+{
+    public partial class CreateNewRequest
+    {
+
+        [Parameter]
+        public string Cdn { get; set; }
+
+        [Inject]
+        public IGatewayService GatewayService { get; set; }
+
+        public MpdisApplicantDto Applicant { get; set; }
+
+        protected override void OnInitialized()
+        {
+            this.Applicant = this.GatewayService.GetApplicantInfoByCdn(Cdn);
+
+        }
+
+
+    }
+}
