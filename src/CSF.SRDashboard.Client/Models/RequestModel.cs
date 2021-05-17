9f486e75f7bfcb466149321516e63e1c5e71a70a
Chris ikongo <chris.ikongo@034gc.onmicrosoft.com>
2021-05-17 11:02:23 -0400

    [AB#86950-Create-New-Request] Created model and page

---
+++ src/CSF.SRDashboard.Client/Models/RequestModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSF.SRDashboard.Client.Models
+{
+    public class RequestModel
+    {
+        public string RequestID { get; set; }
+
+        public string RequestType { get; set; }
+
+        public string CertificateType { get; set; }
+
+        public string SubmissionMethod { get; set; }
+
+        public string ApplicantFullName { get; set; }
+
+        public string Cdn { get; set; }
+
+        public Document Documents { get; set; }
+    }
+}
--- src/CSF.SRDashboard.Client/Pages/CreateNewRequest.razor.cs
+++ src/CSF.SRDashboard.Client/Pages/CreateNewRequest.razor.cs
@@ -1,6 +1,10 @@
 ﻿using CSF.SRDashboard.Client.DTO;
 using CSF.SRDashboard.Client.Services;
 using Microsoft.AspNetCore.Components;
+
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using DSD.MSS.Blazor.Components.Core;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +14,7 @@ namespace CSF.SRDashboard.Client.Pages
 {
     public partial class CreateNewRequest
     {
+        protected EditContext EditContext;
 
         [Parameter]
         public string Cdn { get; set; }
@@ -19,12 +24,22 @@ namespace CSF.SRDashboard.Client.Pages
 
         public MpdisApplicantDto Applicant { get; set; }
 
-        protected override void OnInitialized()
+        protected async override Task OnInitializedAsync()
         {
+            await base.OnInitializedAsync();
+
+            this.Applicant = new MpdisApplicantDto();
+
             this.Applicant = this.GatewayService.GetApplicantInfoByCdn(Cdn);
+
+            this.EditContext = new EditContext(this.Applicant);
 
         }
 
+        void ClearMessages()
+        {
+
+        }
 
     }
 }
