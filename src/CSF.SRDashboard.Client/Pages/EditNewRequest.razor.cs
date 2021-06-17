30f47e0eddff133997a4c5c3556d272b620d1ee7
DarrenMes-tc <darren.mes@tc.gc.ca>
2021-06-17 11:56:33 -0400

    [AB#81412-Update-Document-Metadata-on-a-Request] Fixed cancel button

--- src/CSF.SRDashboard.Client/Pages/EditNewRequest.razor.cs
+++ src/CSF.SRDashboard.Client/Pages/EditNewRequest.razor.cs
@@ -75,6 +75,11 @@ namespace CSF.SRDashboard.Client.Pages
             StateHasChanged();
         }
 
+        public void Cancel()
+        {
+            this.NavigationManager.NavigateTo("/SeafarerProfile/" + Cdn);
+        }
+
         public async void SaveChanges()
         {
             var isValid = EditContext.Validate();
