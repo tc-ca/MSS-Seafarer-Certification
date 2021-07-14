ccb4cfba6df6bb306ee4a0651e901e7ad63e86fd
Chris ikongo <chris.ikongo@034gc.onmicrosoft.com>
2021-07-14 12:21:41 -0400

    [AB#81881-Request-Comments] Added new property

--- src/CSF.SRDashboard.Client/Models/RequestCommentInfo.cs
+++ src/CSF.SRDashboard.Client/Models/RequestCommentInfo.cs
@@ -10,7 +10,8 @@ namespace CSF.SRDashboard.Client.Models
         public int Id { get; set; }
         public int WorkItemId { get; set; }
         public string Comment { get; set; }
-        public System.DateTimeOffset? CreatedDateUTC { get; set; }
+        public DateTimeOffset? CreatedDateUTC { get; set; }
+        public string CreatedDateFormatted { get; set; }
         public string CreatedBy { get; set; }
     }
 }
