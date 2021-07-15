faea122637a1ee6b42f37bccb7442c0ccacfc471
Chris ikongo <chris.ikongo@034gc.onmicrosoft.com>
2021-07-15 15:25:07 -0400

    [AB#81881-Request-Comments] Time shows on comments

--- src/CSF.SRDashboard.Client/Components/Tables/WorkloadRequest/RequestCommentComponent.razor.cs
+++ src/CSF.SRDashboard.Client/Components/Tables/WorkloadRequest/RequestCommentComponent.razor.cs
@@ -5,22 +5,26 @@ using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Forms;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Localization;
 
 namespace CSF.SRDashboard.Client.Components.Tables.WorkloadRequest
 {
     public partial class RequestCommentComponent
     {
         [Parameter]
-        public List<RequestCommentInfo> WorkComments {
+        public List<RequestCommentInfo> WorkComments
+        {
             get => workComments;
-                set
+            set
             {
                 if (workComments == value) return;
                 this.workComments = value;
                 WorkCommentsChanged.InvokeAsync(value);
-            } 
+            }
         }
         [Parameter]
         public bool ReadOnly { get; set; }
@@ -28,6 +32,8 @@ namespace CSF.SRDashboard.Client.Components.Tables.WorkloadRequest
         public EventCallback<string> SubmitComment { get; set; }
         [Parameter]
         public EventCallback<List<RequestCommentInfo>> WorkCommentsChanged { get; set; }
+        [Inject]
+        public IHttpContextAccessor httpContextAccessor { get; set; }
         private SortOption SortOption { get; set; } = new SortOption();
         public EditContext EditContext { get; set; }
         private List<RequestCommentInfo> workComments { get; set; }
@@ -40,47 +46,99 @@ namespace CSF.SRDashboard.Client.Components.Tables.WorkloadRequest
 
         protected override void OnInitialized()
         {
-           
+
+            var language = httpContextAccessor.HttpContext.Request.Cookies[CookieRequestCultureProvider.DefaultCookieName];
+            language = language.Substring(2, 2);
+
+            foreach (var item in WorkComments)
+            {
+                item.CreatedDateFormatted = DateFormat(item.CreatedDateUTC.Value.DateTime, language);
+            }
 
             this.EditContext = new EditContext(this.RequestComment);
             base.OnInitialized();
         }
         public async void SaveComment()
         {
-           
             if (this.EditContext.Validate())
             {
                 await SubmitComment.InvokeAsync(this.RequestComment.Comment);
             }
         }
-        
+
         public void ChangeSortOrder()
         {
-            if(string.Equals(this.SortOption.Text, "2")){
-             var sortedNew = WorkComments.OrderByDescending(o => o.CreatedDateUTC).ToList();
-                this.WorkComments = sortedNew; 
+            if (string.Equals(this.SortOption.Text, "2"))
+            {
+                var sortedNew = WorkComments.OrderByDescending(o => o.CreatedDateUTC).ToList();
+                this.WorkComments = sortedNew;
             }
             else
             {
-             var sortedOld = WorkComments.OrderBy(o => o.CreatedDateUTC).ToList();
+                var sortedOld = WorkComments.OrderBy(o => o.CreatedDateUTC).ToList();
                 this.WorkComments = sortedOld;
             }
             StateHasChanged();
         }
 
         private bool IsCommentListEmpty() => this.WorkComments == null || !this.WorkComments.Any() ? false : true;
-        private TimeSpan GetElapsedTime(DateTime postedTime)
+        private string DateFormat(DateTime dateToFormat, string language, DateTime? currentTime = null)
         {
-            DateTime currentTime = DateTime.UtcNow;
-            TimeSpan timeDifference = currentTime - postedTime;
+            var isFrench = false;
+            DateTime now = DateTime.UtcNow;
 
-            return timeDifference;
+            if (currentTime.HasValue)
+            {
+                now = currentTime.Value;
+            }
 
+            CultureInfo culture = new CultureInfo("en-CA");
 
+            if (language.Equals("Fr", StringComparison.OrdinalIgnoreCase))
+            {
+                culture = new CultureInfo("fr-CA");
+                isFrench = true;
+            }
+
+            Console.WriteLine("Comment = " + dateToFormat);
+
+            var mins = Convert.ToInt32((now - dateToFormat).TotalMinutes); // Less than or = to 59 | 1m - 59m
+            var hour = Convert.ToInt32((now - dateToFormat).TotalHours);
+            var days = Convert.ToInt32((now - dateToFormat).TotalDays);
+
+            string date;
+            if (mins <= 59)
+            {
+                date = string.Format("{0}m", mins);
+            }
+            else if (hour <= 23)
+            {
+                date = string.Format("{0}h", hour);
+            }
+            else if (days <= 6)
+            {
+                var dayString = "d";
+
+                if (isFrench)
+                {
+                    dayString = "j";
+                }
+
+                date = string.Format("{0}{1}", days, dayString);
+            }
+            else
+            {
+                date = dateToFormat.ToString("dd MMM, yyyy", culture);
+                var charsToRemove = new string[] { "." };
+                foreach (var c in charsToRemove)
+                {
+                    date = date.Replace(c, string.Empty);
+                }
+            }
+
+            return date;
         }
     }
-   
-
 
     public partial class SortOption
     {
